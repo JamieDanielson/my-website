95013930b49690b77a5cb978d79a71dfe1792610
Jamie <jamieedanielson@gmail.com>
2020-11-24 21:41:16 -0500

    Fix issue on gtag config

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingId: [
+        trackingIds: [
           `G-MPPJ0PJ8JF`, // Google Analytics / GA
         ],
       },
