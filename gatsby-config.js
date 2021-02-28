2c412ee55c7db2e1fc488bee48f9b939019ae3b5
Jamie <jamieedanielson@gmail.com>
2021-02-28 17:02:59 -0500

    Update Meta description, favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Jamie Danielson`,
     subtitle: `software developer`,
-    description: `I'm Jamie. I learn, I code, I write.`,
+    description: `I'm Jamie Danielson. I learn, I code, I write.`,
     author: `Jamie`,
   },
   plugins: [
@@ -68,10 +68,22 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffffff`,
+        theme_color: `#ffffff`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        // icons: [
+        //   {
+        //     src: 'src/images/icons/android-chrome-192x192.png',
+        //     sizes: '192x192',
+        //     type: 'image/png',
+        //   },
+        //   {
+        //     src: 'src/images/icons/android-chrome-512x512.png',
+        //     sizes: '512x512',
+        //     type: 'image/png',
+        //   },
+        // ],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
